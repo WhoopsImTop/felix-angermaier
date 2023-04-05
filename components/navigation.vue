cc3b917b70d32de7100e02b2fdf78dd3e2576a1c
Lea <eliasenglen2@gmail.com>
2023-04-05 12:02:00 +0200

    add new

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="navigation">
+  <div class="navigation" :class="active ? 'active' : ''">
     <div class="content-container navigation-container">
       <nuxt-link to="/"><klangbild-logo-short height="50px" width="50px"></klangbild-logo-short></nuxt-link>
-      <div class="navigation-items">
-        <nuxt-link to="/" class="navigation-item">Home</nuxt-link>
-        <nuxt-link to="/about" class="navigation-item">About</nuxt-link>
-        <nuxt-link to="/contact" class="navigation-item">Contact</nuxt-link>
+      <div class="navigation-items" @click="active = false">
+        <nuxt-link to="/" class="navigation-item" @click="active = false">Home</nuxt-link>
+        <nuxt-link to="/about" class="navigation-item" @click="active = false">About</nuxt-link>
+        <nuxt-link to="/contact" class="navigation-item" @click="active = false">Contact</nuxt-link>
+      </div>
+      <div class="burger" :class="active ? 'active' : ''" @click="active = !active">
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
       </div>
     </div>
   </div>
@@ -15,6 +20,11 @@
 import klangbildLogoShort from "./klangbildLogoShort.vue";
 export default {
   components: { klangbildLogoShort },
+  data() {
+    return {
+      active: false,
+    };
+  },
 };
 </script>
 
