d6db47079b61ce39eb74e57c5ff60f18ed5e615e
WhoopsImTop <eliasenglen2@gmail.com>
2023-08-09 13:08:41 +0200

    add new

--- store/index.js
+++ store/index.js
@@ -11,6 +11,8 @@ export const mutations = {
 export const actions = {
     async getPortfolioItems({ commit }) {
         const res = await this.$content("portfolio").fetch();
+        //order by position
+        res.sort((a, b) => (a.position > b.position ? b.position : -1));
         commit('setPortfolioItems', res);
     }
 };
